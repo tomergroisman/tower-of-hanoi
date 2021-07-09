7a8331268fa1de9d3bbd9addf87b28c31a5838b7
Tomer Groisman <tomergroisman@gmail.com>
2021-07-09 14:38:47 +0300

    (client) Redirect unauthenticated users to signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import {Provider} from 'react-redux';
+import {useCookies} from 'react-cookie';
+import {Redirect} from 'react-router-dom';
 
 import {store} from './store';
 import {Router} from './components/Router';
 
 function App() {
+  const [cookies] = useCookies(['token']);
+
   return (
     <Provider store={store}>
       <Router />
+      {!cookies.token && <Redirect to="/signup" />}
     </Provider>
   );
 }
