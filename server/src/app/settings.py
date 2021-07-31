c38e51fc619455b6552dc980392c074598e6712b
Tomer Groisman <tomergroisman@gmail.com>
2021-07-31 10:27:28 +0300

    (server) Add default authentication class

--- server/src/app/settings.py
+++ server/src/app/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20
 }
