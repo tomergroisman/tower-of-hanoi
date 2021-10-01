be9603fc729ab36ffc936d68c4c7dd1707b4b94b
Tomer Groisman <tomergroisman@gmail.com>
2021-10-01 10:59:50 +0300

    Add build enviornment variable to client service

--- client/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# line endings must be \n, not \r\n !
-echo "window._env_ = {" > ./build/env-config.js
-echo 'API_ENDPOINT:' '"'$API_ENDPOINT'"'',' >> ./build/env-config.js
-echo "}" >> ./build/env-config.js
